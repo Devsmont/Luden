17576b66a72b11ffd262adf4e41693f8ac4cfa37
Devsmont <davimonteirodossantos@hotmail.com>
2024-06-14 11:02:45 -0300

    Concertando os Links e Colocando a base do carrosel

--- Luden-Application/Luden/scripts/carousel-owl/setup.js
+++ Luden-Application/Luden/scripts/carousel-owl/setup.js
@@ -1,19 +1,29 @@
 // código informado na documentação do Owl Carousel 2
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-
-    // responsivo
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:false,
+    nav:true,
+    margin:10,
     responsive:{
         0:{
             items:1
         },
         600:{
             items:3
-        },
-        1000:{
+        },            
+        960:{
             items:5
+        },
+        1200:{
+            items:6
         }
     }
-})
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
